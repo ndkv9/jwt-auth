c309164e02b0333c484fed4ba6914345f209f509
Vu Nguyen <vu.nguyen101@gmail.com>
2020-09-18 16:59:03 +0300

    add images

--- utils/middleware.js
+++ utils/middleware.js
@@ -44,11 +44,9 @@ const checkUser = (req, res, next) => {
 	if (token) {
 		jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
 			if (err) {
-				console.log(err.message)
 				res.locals.user = null
 				next()
 			} else {
-				console.log(decodedToken)
 				let user = await User.findById(decodedToken.id)
 				res.locals.user = user
 				next()
