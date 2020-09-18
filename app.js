fe74bcd675d518536565fea023dd68bb1035c061
Vu Nguyen <vu.nguyen101@gmail.com>
2020-09-18 08:16:13 +0300

    good morning sir

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
+
 app.use('/', authRoutes)
 
 // view engine
@@ -30,6 +33,22 @@ mongoose
 // routes
 app.get('/', (req, res) => res.render('home'))
 app.get('/smoothies', (req, res) => res.render('smoothies'))
+
+app.get('/set-cookies', (req, res) => {
+	//res.setHeader('Set-Cookie', 'newUser=true')
+
+	res.cookie('newUser', false)
+	res.cookie('isEmployee', true, {
+		maxAge: 1000 * 60 * 60 * 24,
+		httpOnly: true,
+	})
+	res.send('you got the cookies')
+})
+
+app.get('/get-cookies', (req, res) => {
+	const cookies = req.cookies
+	res.json(cookies)
+})
 
 app.use(middleware.unknownEnpoint)
 app.use(middleware.errorHandler)
